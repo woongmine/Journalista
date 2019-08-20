9eacbd1b037d4f9ee039ea62532f2d50e423fcec
vivala0519 <42686784+vivala0519@users.noreply.github.com>
2019-08-20 16:36:06 +0900

    누락파일 추가

---
+++ src/main/java/kr/co/journalista/LikeVO.java
@@ -0,0 +1,34 @@
+package kr.co.journalista;
+
+public class LikeVO {
+	private int like_no;
+	private int m_no;
+	private int e_no;
+	private int like_check;
+	
+	
+	public int getLike_no() {
+		return like_no;
+	}
+	public void setLike_no(int like_no) {
+		this.like_no = like_no;
+	}
+	public int getM_no() {
+		return m_no;
+	}
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
+	}
+	public int getE_no() {
+		return e_no;
+	}
+	public void setE_no(int e_no) {
+		this.e_no = e_no;
+	}
+	public int getLike_check() {
+		return like_check;
+	}
+	public void setLike_check(int like_check) {
+		this.like_check = like_check;
+	}
+}
