141bec01bc61415fe5e30f676b4260b72b5e2ab9
woongmine <woongmine@gmail.com>
2019-09-04 10:59:47 +0900

    비밀번호 찾기 이메일 인증 ui 변경

--- src/main/java/kr/co/journalista/member/MemberServiceImpl.java
+++ src/main/java/kr/co/journalista/member/MemberServiceImpl.java
@@ -76,11 +76,15 @@ public class MemberServiceImpl implements MemberService {
 		MailHandler sendMail = new MailHandler(mailSender);
 		sendMail.setSubject("[Journalista 임시비밀번호 발급]");
 		sendMail.setText(
-				new StringBuffer().append("<h1> 저널리스트 임시비밀번호 입니다. </h1>").append("이메일은 :  "+vo.getEmail()+"<br>").append("임시비밀번호는  : "+vo.getPassword()).toString());
+				new StringBuffer()
+				.append("<table class=\"__se_tbl\" border=\"0\" cellpadding=\"0\" cellspacing=\"1\" style=\"background-color: rgb(199, 199, 199);\"><tbody><tr><td style=\"padding: 3px 4px 2px; background-color: rgb(255, 255, 255); color: rgb(102, 102, 102); width: 853px; height: 282px;\"><p>&nbsp;</p><h2 style=\"margin: 0px 0px 1em; padding: 0px; font-style: inherit; font-variant: inherit; box-sizing: inherit; line-height: 1.65; font-size: 1.1em; border: 0px; font-stretch: inherit; font-family: Raleway, Helvetica, sans-serif; vertical-align: baseline; color: rgb(60, 59, 59); letter-spacing: 0.25em; text-transform: uppercase; text-align: center;\"><span style=\"font-size: 14pt; color: rgb(37, 37, 37);\">JOURNALISTA 임시 비밀번호 발급</span></h2><h2 style=\"margin: 0px 0px 1em; padding: 0px; font-style: inherit; font-variant: inherit; box-sizing: inherit; line-height: 1.65; font-size: 1.1em; border: 0px; font-stretch: inherit; font-family: Raleway, Helvetica, sans-serif; vertical-align: baseline; color: rgb(60, 59, 59); letter-spacing: 0.25em; text-transform: uppercase; text-align: center;\"><div style=\"box-sizing: inherit; margin: 0px; padding: 0px; border: 0px; font-variant-numeric: inherit; font-variant-east-asian: inherit; font-weight: 400; font-stretch: inherit; font-size: 18.6667px; line-height: inherit; font-family: Source Sans Pro; vertical-align: baseline; color: rgb(100, 100, 100); letter-spacing: normal; text-align: left; text-transform: none;\"></div><div style=\"box-sizing: inherit; margin: 0px; padding: 0px; border: 0px; font-variant-numeric: inherit; font-variant-east-asian: inherit; font-stretch: inherit; font-size: 18.6667px; line-height: inherit; font-family: Source Sans Pro; vertical-align: baseline; color: rgb(100, 100, 100); letter-spacing: normal; text-transform: none;\"><p style=\"font-style: inherit; font-variant: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-family: inherit; margin-bottom: 2em; box-sizing: inherit; border: 0px; vertical-align: baseline; color: rgb(170, 170, 170);\"><span style=\"font-weight: inherit;\">임시 비밀번호는 ")
+				.append("</span><span style=\"color: rgb(0, 0, 0);\"><u>"+vo.getPassword()+"</u></span><span style=\"font-weight: inherit;\">입니다.</span></p></div></h2></td></tr></tbody>\r\n" + 
+						"</table>")
+				.toString());
 		sendMail.setFrom("Journalista", "저널리스타");
 		sendMail.setTo(vo.getEmail());
 		sendMail.send();
-		String spassword = vo.getPassword();					
+		String spassword = vo.getPassword();
 		String encryptPassword = passwordEncoder.encode(spassword);	
 		vo.setPassword(encryptPassword);
 		dao.update_pw(vo);
